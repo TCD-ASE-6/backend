ea3bed221a7ec05e80de3a5945cf9cef012663bf
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-13 12:34:12 +0100

    3

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const passport = require("passport");
 const config = require("config");
 const path = require("path");
 const request = require("request");
-const createHealthCheckManager =
-  require("./loadBalancers/health-checks/index").createHealthCheckManager;
+const createHealthCheckManager = require("./loadBalancers/health-checks/index").createHealthCheckManager
 
 //APIs
 const directionsAPI = require("./routes/api/directions");
 const incidentsAPI = require("./routes/api/reportDisasterAPI");
 const usersAPI = require("./routes/api/users");
-const WeightedRoundRobin = require("./loadBalancers/weightedRoundRobin");
+const bikesAPI = require("./routes/api/dublinBikesAPI");
+const WeightedRoundRobin  = require("./loadBalancers/weightedRoundRobin");
 
 const app1 = express();
 const app2 = express();
@@ -47,11 +47,10 @@ const weightedRoundRobinHandler = (req, res) => {
 };
 
 // Get MongoDbURI according to ENV script
-// NODE_ENV = "production"
-const db = config.get("mongoURI");
-// process.env.NODE_ENV.trim() === "production"
-//   ? config.get("mongoURI")
-//   : config.get("devMongoURI");
+const db = config.get("mongoURI")
+  // process.env.NODE_ENV.trim() === "production"
+    // ? config.get("mongoURI")
+    // : config.get("devMongoURI");
 
 // Connect to MongoDB
 mongoose
@@ -81,107 +80,93 @@ app2.use("/api/directions", directionsAPI);
 app1.use("/api/incident", incidentsAPI);
 app2.use("/api/incident", incidentsAPI);
 
-// app1.use(
-//   express.static(path.join(__dirname, "../client/build"))
-// );
-// app2.use(
-//   express.static(path.join(__dirname, "../client/build"))
-// );
-// // Catch all requests that don't match any route
-// app1.get("*", (req, res) => {
-//   res.sendFile(
-//     path.join(__dirname, "../client/build/index.html")
-//   );
-// });
-// app2.get("*", (req, res) => {
-//   res.sendFile(
-//     path.join(__dirname, "../client/build/index.html")
-//   );
-// });
+app1.use("/api/bikes", bikesAPI);
+app2.use("/api/bikes", bikesAPI);
+
+app1.use(
+  express.static(path.join(__dirname, "../client/build"))
+);
+app2.use(
+  express.static(path.join(__dirname, "../client/build"))
+);
+// Catch all requests that don't match any route
+app1.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html")
+  );
+});
+app2.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html")
+  );
+});
 
 // const port = process.env.PORT || 8080;
-const server = express()
-  .get("*", weightedRoundRobinHandler)
-  .post("*", weightedRoundRobinHandler);
 
-server.use(cors());
+const server = express().get("*", weightedRoundRobinHandler).post("*", weightedRoundRobinHandler).put("*", weightedRoundRobinHandler)
+server.use(cors())
 
 const beforeShutdownCallback = () => {
-  console.log("Shutdown in Progress");
-};
+  console.log("Shutdown in Progress")
+}
 
 const onShutdownCallback = () => {
-  console.log("Shutdown has occured");
-};
+  console.log("Shutdown has occured")
+}
 
 const healthCheckLogger = (log, error) => {
-  console.log("Log from healthcheck...", log);
-  console.log("Error from healthcheck...", error);
-};
+  console.log("Log from healthcheck...", log)
+  console.log("Error from healthcheck...", error)
+}
 
 const onSendFailureWithShutdownLogger = () => {
-  console.log("Sending failure has occcured for Shutdown ");
-};
+  console.log("Sending failure has occcured for Shutdown ")
+}
 
-const livenessCheck = () =>
-  new Promise((resolve, _reject) => {
-    // TODO: Need to add logic overhere to set app functioning correctly
-    const appFunctioning = true;
-    if (appFunctioning) {
-      resolve();
-    } else {
-      reject(new Error("App is not functioning correctly"));
-    }
-  });
+const livenessCheck = () => new Promise((resolve, reject) => {
+  // TODO: Need to add logic overhere to set app functioning correctly
+  const appFunctioning = true;
+  if (appFunctioning) {
+    resolve();
+  } else {
+    reject(new Error("App is not functioning correctly"));
+  }
+});
 
 /**
  * Code to support graceful shutdown for the database in case shutdown occurs
  */
 
-function onSigterm() {
-  console.log("server is starting cleanup");
+function onSigterm () {
+  console.log('server is starting cleanup')
   // Adding graceful shudown for database
-  return Promise.all([mongoose.connection.close()]);
+  return Promise.all([
+    mongoose.connection.close()
+  ])
 }
 
-const options = {
+const options = { 
   timeout: 1000,
   onShutdown: onShutdownCallback,
   beforeShutdown: beforeShutdownCallback,
   healthChecks: {
-    "/health/liveness": livenessCheck,
+   "/health/liveness": livenessCheck,
     __unsafeExposeStackTraces: true,
-    verbatim: true,
+    verbatim: true, 
   },
   sendFailuresWithShutdown: true,
   onSendFailureWithShutdown: onSendFailureWithShutdownLogger,
   logging: healthCheckLogger,
-  statusSuccess: 200,
+  statusSuccess:200,
   statusError: 500,
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
-  onSignal: onSigterm,
-};
+  onSignal: onSigterm
+} 
 createHealthCheckManager(server, options);
 
-// const optionsHTTPS = {
-//   key: fs.readFileSync("./localhost-key.pem"),
-//   cert: fs.readFileSync("./localhost.pem"),
-//   // headers: {
-//   //   'Access-Control-Allow-Origin': '*',
-//   //   'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-//   //   'Access-Control-Max-Age': 2592000, // 30 days
-//   //   /** add other headers as per requirement */
-//   // }
-// };
-
 server.listen(8080);
-// https.createServer(optionsHTTPS, server).listen(8080, function () {
-//   console.log(
-//     "Example app listening on port 8080! Go to https://localhost:8080/"
-//   );
-// });
 
 app1.listen(3004, () => console.log(`server started on port 3004`));
 app2.listen(3005, () => console.log(`server started on port 3005`));
